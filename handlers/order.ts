72ad8d4b9c727ea5d3b805cea529929183baf119
greevashah <greeva14@gmail.com>
2024-06-05 23:22:10 +0530

    try-catch

--- handlers/order.ts
+++ handlers/order.ts
@@ -2,21 +2,28 @@ import { Product, Order } from "../type";
 import { getAllProducts, getProductFromId, updateProductFromId } from "./product";
 
 export const createOrder = (requestedProducts: Product[]): Order => {
-    const products = getAllProducts();
-    const ids: number[] = products.map((p) => p.id);
-    const finalOrder: Order = { price: 0, products: [], address: ""};
-    requestedProducts.forEach((p) => {
-        if(ids.includes(p.id)){
-            const matchedProduct: Product | null = getProductFromId(p.id);
-            if(matchedProduct && matchedProduct.quantity) {
-                const curQuantity = matchedProduct.quantity;
-                if(curQuantity && curQuantity > 1) {
+    try {
+        const products = getAllProducts();
+        const ids: number[] = products.map((p) => p.id);
+        const finalOrder: Order = { price: 0, products: [], address: ""};
+        requestedProducts.forEach((p) => {
+            if(ids.includes(p.id)){
+                const matchedProduct: Product = getProductFromId(p.id);
+                if(matchedProduct) {
+                    if(!matchedProduct.quantity) {
+                        throw new Error("Quantity of requested product is 0");
+                    }
+                    else if(!matchedProduct.isActive) {
+                        throw new Error("Requested product is inactive")
+                    }
                     matchedProduct.quantity -= 1;
                     updateProductFromId(p.id, matchedProduct);
                     finalOrder.products.push(p);
                 }
             }
-        }
-    })
-    return finalOrder;
+        })
+        return finalOrder;
+    } catch (err) {
+        throw err;
+    }
 }
