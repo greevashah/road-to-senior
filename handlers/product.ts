8c46a0df9ca7906fccb312b6b9f43725f0479e63
greevashah <greeva14@gmail.com>
2024-06-08 15:05:58 +0530

    promise-in-methods

--- handlers/product.ts
+++ handlers/product.ts
@@ -1,22 +1,24 @@
 import {products} from "../db/product"
 import { Product } from "../type"
 
-export const getAllProducts = (): Product[]  => {
-    return products.map((p) => (Object.assign({}, p)) )
+export const getAllProducts = (): Promise<Product[]>  => {
+    return Promise.resolve(products.map((p) => (Object.assign({}, p)) ))
 }
 
-export const getProductFromId = (id:number): Product => {
-    const matchedProduct = products.filter((p) => p.id === id);
-    if(matchedProduct.length == 0){
-        throw new Error("Product not found");
-    }
-    return Object.assign({}, matchedProduct[0]);
+export const getProductFromId = (id:number): Promise<Product> => {
+    return new Promise((resolve, reject) => {
+        const matchedProduct = products.filter((p) => p.id === id);
+        if(matchedProduct.length == 0){
+            reject(new Error("Product not found"));
+        }
+        resolve(Object.assign({}, matchedProduct[0]));
+    });
 }
 
-export const updateProductFromId = (id: number, updatedProduct: Product) => {
+export const updateProductFromId = async (id: number, updatedProduct: Product) => {
     const productIndex = products.findIndex((element) => element.id === id);
     if((productIndex == -1)){
-        throw new Error("Update not possible as product not found");
+        Promise.reject(new Error("Update not possible as product not found"));
     }
     products[productIndex] = {...updatedProduct}
 }
--- main.ts
+++ main.ts
@@ -1,28 +1,24 @@
 import { createOrder } from "./handlers/order";
-import { products } from "./db/product";
-// master list of products inventory, in function array of products in input, subtract the demand from investory
 
-try {
-    const firstOrder = createOrder([
-        {
-            id: 2,
-        },
-        {
-            id: 3,
-        },
-        {
-            id: 1,
-        },
-        {
-            id: 4,
-        },
-    ]);
-
-    console.log("Products: ", products);
-    console.log("First Order is", firstOrder);
-}
-catch (error) {
-    console.log(error);
-}
-
-// promises / error handling
+(async() => {
+    try {
+        const firstOrder = await createOrder([
+            {
+                id: 2,
+            },
+            {
+                id: 3,
+            },
+            {
+                id: 1,
+            },
+            {
+                id: 4,
+            },
+        ]);
+        console.log("First Order is", firstOrder);
+    }
+    catch (error) {
+        console.log(error);
+    }
+})()
